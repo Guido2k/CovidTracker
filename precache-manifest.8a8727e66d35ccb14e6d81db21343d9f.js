f2b5338b3e4906e0dded1d183f373765957f2a4f
Guido2k <Krypto.taco@gmail.com>
2020-07-15 15:05:36 -0600

    Updates

---
+++ precache-manifest.8a8727e66d35ccb14e6d81db21343d9f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7de61960430914e7d25c9b50403b4abc",
+    "url": "/CovidTracker/index.html"
+  },
+  {
+    "revision": "e1a683ab6ba72a8edd60",
+    "url": "/CovidTracker/static/css/main.cb5361df.chunk.css"
+  },
+  {
+    "revision": "cb5bb16bfb1509965b9c",
+    "url": "/CovidTracker/static/js/2.bac25390.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/CovidTracker/static/js/2.bac25390.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e1a683ab6ba72a8edd60",
+    "url": "/CovidTracker/static/js/main.b3f9845e.chunk.js"
+  },
+  {
+    "revision": "b587074c51c32844276b",
+    "url": "/CovidTracker/static/js/runtime-main.1cccd4fc.js"
+  },
+  {
+    "revision": "14126a3043a00ad8fde94b3efa39da21",
+    "url": "/CovidTracker/static/media/corona2bg.14126a30.png"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/CovidTracker/static/media/image.d7265326.png"
+  }
+]);
